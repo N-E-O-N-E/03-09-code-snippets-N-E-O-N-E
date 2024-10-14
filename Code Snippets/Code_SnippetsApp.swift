50057c1329bdc55fe52e74e86d449b07a85df499
Markus Wirtz <info@markuswirtz.net>
2024-10-14 16:38:49 +0200

    Datensicherung von NEONE

--- Code Snippets/Code_SnippetsApp.swift
+++ Code Snippets/Code_SnippetsApp.swift
@@ -1,20 +1,30 @@
 import SwiftUI
+import FirebaseCore
 import Firebase
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 
 @main
 struct Code_SnippetsApp: App {
     
-    init() {
-        FirebaseConfiguration.shared.setLoggerLevel(.min)
-        FirebaseApp.configure()
-    }
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         
         WindowGroup {
             
             TestButton()
+            
         }
     }
 }
